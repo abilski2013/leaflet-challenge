6ea522a06b445bbc195fdfacce1a8d7e3ee0c07c
Adam Bilski <abilski2013@gmail.com>
2020-04-10 13:32:55 -0500

    step 2 homework complete

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,24 +1,40 @@
-// Create a map object
-var myMap = L.map("map", {
-  center: [37.09, -95.71],
-  zoom: 5
-});
-
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.pirates",
-  accessToken: API_KEY
-}).addTo(myMap);
-
-
-d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson', function(data) {
-    console.log(data);
+function createMap(earthquakes, lines) {
     
-    function markerSize(magnitude) {
-        return magnitude * 20000;
-    }
-      
+    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
+    maxZoom: 15,
+    id: "mapbox.light",
+    accessToken: API_KEY
+    });
+    
+    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
+    maxZoom: 15,
+    id: "mapbox.dark",
+    accessToken: API_KEY
+    });
+
+    var baseMaps = {
+    "Light Map": lightmap,
+    "Dark Map": darkmap
+    };    
+    
+    var overlayMaps = {
+    "Earth Quakes": earthquakes,
+    "Fault Lines": lines
+    }; 
+    
+    var map = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5,
+    layers: [lightmap, earthquakes]
+    });    
+    
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(map);
+    
+    var legend = L.control({position: 'bottomright'});
     
     function color(magnitude) {
     return magnitude < 1 ? '#DA70D6' :
@@ -27,30 +43,13 @@ d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
            magnitude < 6 ? '#8B008B' :
                            '#4B0082' ;
     };
-        
-//    console.log(data.features[0].properties.mag)
-//    console.log(data.features[0].geometry.coordinates[1])
-//    console.log(data.features[0].geometry.coordinates[0])
-//    
-    for (var i=0; i < data.features.length; i++) {
-        L.circle([data.features[i].geometry.coordinates[1], data.features[i].geometry.coordinates[0]], {
-            fillOpacity: .75,
-            color: "white",
-            weight: .5,
-            fillColor: color(data.features[i].properties.mag),
-            radius: markerSize(data.features[i].properties.mag) 
-        }).bindPopup("<h3>" + data.features[i].properties.title + "<br><p class='link'><a href='" + data.features[i].properties.url + "' target='_blank'>" + data.features[i].properties.url + "</a></p></h3>").addTo(myMap);
-    }
     
-    var legend = L.control({position: 'bottomright'});
-
-    legend.onAdd = function (myMap) {
+    legend.onAdd = function (map) {
 
         var div = L.DomUtil.create('div', 'info legend'),
             grades = [Infinity, 6, 4.5, 2.5, 1],
             labels = [];
 
-        // loop through our density intervals and generate a label with a colored square for each interval
         for (var i = 0; i < grades.length; i++) {
             div.innerHTML +=
                 '<i style="background:' + color(grades[i] - .1) + '"></i> ' +
@@ -59,7 +58,65 @@ d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 
         return div;
     };
+    
+    legend.addTo(map);
+}
 
-    legend.addTo(myMap);
+function createCircles(data) {
+    
+    console.log(data);
+    
+    function markerSize(magnitude) {
+        return magnitude * 20000;
+    } 
+    
+    function color(magnitude) {
+    return magnitude < 1 ? '#DA70D6' :
+           magnitude < 2.5 ? '#BA55D3' :
+           magnitude < 4.5 ? '#8A2BE2' :
+           magnitude < 6 ? '#8B008B' :
+                           '#4B0082' ;
+    };
 
-});
+    earthquakes = []
+    
+    for (var i=0; i < data.features.length; i++) {
+        var quake = L.circle([data.features[i].geometry.coordinates[1], data.features[i].geometry.coordinates[0]], {
+            fillOpacity: .75,
+            color: "white",
+            weight: .5,
+            fillColor: color(data.features[i].properties.mag),
+            radius: markerSize(data.features[i].properties.mag) 
+        }).bindPopup("<h3>" + data.features[i].properties.title + "<br><p class='link'><a href='" + data.features[i].properties.url + "' target='_blank'>" + data.features[i].properties.url + "</a></p></h3>");
+        earthquakes.push(quake);
+    }
+    
+    d3.json('https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json', function (data) {
+        function coords(line) {
+            var newLine = []
+            for (j=0; j<line.length; j++) {
+                var newpoints = [];
+                var secpoint = line[j][1];
+                var firpoint = line[j][0];
+                var list = [secpoint, firpoint];
+                newLine.push(list);
+            };
+            return newLine;
+        };
+
+        var lines = []
+
+        for (i=0; i < data.features.length; i++) {
+            var line = data.features[i].geometry.coordinates;
+            var plotline = L.polyline(coords(line), {
+                color: "yellow",
+                fillColor: "yellow",
+                fillOpacity: 0.75
+            });
+            lines.push(plotline);
+        }
+        createMap(L.layerGroup(earthquakes), L.layerGroup(lines));
+    });
+};
+
+d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson', createCircles);
